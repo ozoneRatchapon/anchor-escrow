b7d1137e71cdff497ec11c90bebeedde399847a3
ozoneRatchapon <61614162+ozoneRatchapon@users.noreply.github.com>
2025-04-16 18:49:08 +0700

    feat: add Take instruction with deposit and withdraw functionality

--- programs/anchor-escrow/src/instructions/mod.rs
+++ programs/anchor-escrow/src/instructions/mod.rs
@@ -2,7 +2,7 @@ pub mod make;
 pub use make::*;
 
 pub mod take;
-// pub use take::*;
+pub use take::*;
 
 pub mod refund;
 pub use refund::*;
--- programs/anchor-escrow/src/lib.rs
+++ programs/anchor-escrow/src/lib.rs
@@ -16,15 +16,20 @@ pub mod anchor_escrow {
     // make
     // - init escrow , deposit
     pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
-        ctx.accounts.deposit(deposit);
-        Ok(())
+        ctx.accounts.deposit(deposit)?;
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)
     }
 
     // refund
     // - deposit , close
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
+    }
+
     // take
     // - deposit, withdraw, close vault
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
+    }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
